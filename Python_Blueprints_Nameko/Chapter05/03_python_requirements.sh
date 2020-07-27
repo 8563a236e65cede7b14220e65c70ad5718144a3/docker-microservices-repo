08651de7742aed9df6cb5aeeb0470813bfddcb48
Warren Jitsing <warren.jitsing@gmail.com>
2020-07-27 13:47:06 +0200

    Finish Python requirements
    See: Python Programming Blueprints Chapter 5
    
    Why: Prepare project to use nameko
    
    How: pip
    
    Tags: pip nameko

---
+++ Python_Blueprints_Nameko/Chapter05/03_python_requirements.sh
@@ -0,0 +1,14 @@
+# Install pip-tools
+pip install pip-tools
+
+# Create requirements folder
+mkdir requirements
+
+# Create base.in and test.in
+echo nameko > requirements/base.in
+echo pytest > requirements/test.in
+
+# Compile requirements
+pip-compile requirements/base.in
+pip-compile requirements/test.in
+
